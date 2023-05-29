4a61ca30863ab0c6735c6652058056451ba6ad9b
furtado-matheus <matheusfurtado2797@gmail.com>
2023-05-29 01:42:26 -0300

    Commit do desafio

--- index.js
+++ index.js
@@ -1,65 +1,67 @@
 const express = require('express');
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Olá Mundo!');
+  const endpoint = '/';
+  const method = req.method;
+  const queries = req.query;
+  res.send(`Endpoint: ${endpoint}\nMethod: ${method}\nQueries: ${JSON.stringify(queries)}`);
 });
 
-// Rota /clientes
 app.route('/clientes')
-    .get((req, res) => {
-        const endpoint = '/clientes';
-        const method = req.method;
-        const queries = req.query;
-        res.send(`Endpoint: ${endpoint}\nMethod: ${method}\nQueries: ${JSON.stringify(queries)}`);
-    })
-    .post((req, res) => {
-        const endpoint = '/clientes';
-        const method = req.method;
-        const queries = req.query;
-        res.send(`Endpoint: ${endpoint}\nMethod: ${method}\nQueries: ${JSON.stringify(queries)}`);
-    })
-    .put((req, res) => {
-        const endpoint = '/clientes';
-        const method = req.method;
-        const queries = req.query;
-        res.send(`Endpoint: ${endpoint}\nMethod: ${method}\nQueries: ${JSON.stringify(queries)}`);
-    })
-    .delete((req, res) => {
-        const endpoint = '/clientes';
-        const method = req.method;
-        const queries = req.query;
-        res.send(`Endpoint: ${endpoint}\nMethod: ${method}\nQueries: ${JSON.stringify(queries)}`);
-    });
+  .get((req, res) => {
+    const endpoint = '/clientes';
+    const method = req.method;
+    const queries = req.query;
+    res.send(`Endpoint: ${endpoint}\nMethod: ${method}\nQueries: ${JSON.stringify(queries)}`);
+  })
+  .post((req, res) => {
+    const endpoint = '/clientes';
+    const method = req.method;
+    const queries = req.query;
+    res.send(`Endpoint: ${endpoint}\nMethod: ${method}\nQueries: ${JSON.stringify(queries)}`);
+  })
+  .put((req, res) => {
+    const endpoint = '/clientes';
+    const method = req.method;
+    const queries = req.query;
+    res.send(`Endpoint: ${endpoint}\nMethod: ${method}\nQueries: ${JSON.stringify(queries)}`);
+  })
+  .delete((req, res) => {
+    const endpoint = '/clientes';
+    const method = req.method;
+    const queries = req.query;
+    res.send(`Endpoint: ${endpoint}\nMethod: ${method}\nQueries: ${JSON.stringify(queries)}`);
+  });
 
-// Rota /produtos
 app.route('/produtos')
-    .get((req, res) => {
-        const endpoint = '/produtos';
-        const method = req.method;
-        const queries = req.query;
-        res.send(`Endpoint: ${endpoint}\nMethod: ${method}\nQueries: ${JSON.stringify(queries)}`);
-    })
-    .post((req, res) => {
-        const endpoint = '/produtos';
-        const method = req.method;
-        const queries = req.query;
-        res.send(`Endpoint: ${endpoint}\nMethod: ${method}\nQueries: ${JSON.stringify(queries)}`);
-    })
-    .put((req, res) => {
-        const endpoint = '/produtos';
-        const method = req.method;
-        const queries = req.query;
-        res.send(`Endpoint: ${endpoint}\nMethod: ${method}\nQueries: ${JSON.stringify(queries)}`);
-    })
-    .delete((req, res) => {
-        const endpoint = '/produtos';
-        const method = req.method;
-        const queries = req.query;
-        res.send(`Endpoint: ${endpoint}\nMethod: ${method}\nQueries: ${JSON.stringify(queries)}`);
-    });
+  .get((req, res) => {
+    const endpoint = '/produtos';
+    const method = req.method;
+    const queries = req.query;
+    res.send(`Endpoint: ${endpoint}\nMethod: ${method}\nQueries: ${JSON.stringify(queries)}`);
+  })
+  .post((req, res) => {
+    const endpoint = '/produtos';
+    const method = req.method;
+    const queries = req.query;
+    res.send(`Endpoint: ${endpoint}\nMethod: ${method}\nQueries: ${JSON.stringify(queries)}`);
+  })
+  .put((req, res) => {
+    const endpoint = '/produtos';
+    const method = req.method;
+    const queries = req.query;
+    res.send(`Endpoint: ${endpoint}\nMethod: ${method}\nQueries: ${JSON.stringify(queries)}`);
+  })
+  .delete((req, res) => {
+    const endpoint = '/produtos';
+    const method = req.method;
+    const queries = req.query;
+    res.send(`Endpoint: ${endpoint}\nMethod: ${method}\nQueries: ${JSON.stringify(queries)}`);
+  });
 
 app.listen(port, () => {
-    console.log(`Aplicação exemplo rodando em http://localhost:${port}`);
+  console.log(`Aplicação exemplo rodando em http://localhost:${port}`);
 });
